55d83cce6706e29d46e61c3a258cc720c8578d65
Oliver Dahl <oliverpdahl@gmail.com>
2020-02-04 02:43:02 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,11 +2,11 @@ class Pokemon
   attr_accessor :name, :type
   attr_reader :id, :db
 
-  def initialize(:id = nil, :name, :type, :db = DB[:conn])
-    @id = :id
-    @name = :name
-    @type = :type
-    @db = :db
+  def initialize(id: nil, name:, type:, db: DB[:conn])
+    @id = id
+    @name = name
+    @type = type
+    @db = db
   end
 
   def self.save(:name, :type, :db)
