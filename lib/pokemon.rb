a6a095036631101832cc18d8c9bbf49baebcb506
Oliver Dahl <oliverpdahl@gmail.com>
2020-02-04 02:38:45 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,21 +3,21 @@ class Pokemon
   attr_reader :id, :db
 
   def initialize(:id = nil, :name, :type, :db = DB[:conn])
-    @id = :id 
-    @name = :name 
-    @type = :type 
-    @db = :db 
+    @id = :id
+    @name = :name
+    @type = :type
+    @db = :db
   end
 
   def self.save(:name, :type, :db)
     if self.id
       self.update
     else
-      sql = <<-SQL 
+      sql = <<-SQL
           INSERT INTO pokemon (name, type) VALUES (?, ?)
       SQL
 
       :db.execute(sql, :name, :type)
     end
-    
+
 end
